a51a7d56db6e6338d63992db15132fa9d5845a30
emmanuel-bautista <emmanuel_bautista_morales@outlook.com>
2021-06-17 10:07:27 -0500

    Crear migraciones

---
+++ database/migrations/2021_06_16_063755_create_solicitudes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolicitudesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->id();
+            $table->string('prioridad');
+            $table->string('operacion');
+            $table->string('maquina');
+            $table->string('modulo');
+            $table->foreign('problema_id')->references('id')->on('problemas');
+            $table->date('llegada_mecanico');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+}
