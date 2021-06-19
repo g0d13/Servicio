bb219c041ee287a0ac6c58d271a245d367d2d752
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-06-19 11:15:06 -0500

    mostrar avatar y nombre de usuario logueado

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -10,23 +10,24 @@
                     data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
                 <i class="fa fa-bars" aria-hidden="true"></i>
             </button>
-            {{$slot}}
+            <span class="fw-bold">{{$slot}}</span>
         </div>
         <div class="d-flex align-items-center">
             <div class="d-flex align-items-center justify-content-center me-3 flex-column d-none d-md-block">
-                <span class="m-0 p-0 fw-bold">Margot Robbie</span>
+                <span class="m-0 p-0 fw-bold">{{Auth::user()->name ?? 'Margot Robbie'}}</span>
             </div>
             <div class="btn-group dropstart d-flex">
                 <div class="btn p-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
                      aria-expanded="false">
-                    <img
-                        src="https://graziamagazine.com/mx/wp-content/uploads/sites/13/2019/01/MARGOT-ROBBIE.jpg?fit=1500%2C1000"
-                        alt="" class="rounded-pill" style="width: 40px; height: 40px;">
+                    <div class="avatar avatar-xl">
+                        <div style="height: 40px;width: 40px; background-color: darkslategray;text-align: center;color: white;font-weight: bold;text-transform: uppercase;border-radius: 50%;padding: 10px">
+                            <span>{{substr(Auth::user()->name ?? '', 0, 1)}}</span>
+                        </div>
+                    </div>
                 </div>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><a class="dropdown-item" href="{{route('configuracion')}}">Configuracion</a></li>
+                    <li><a class="dropdown-item" href="{{route('logout')}}">Salir</a></li>
                 </ul>
             </div>
         </div>
