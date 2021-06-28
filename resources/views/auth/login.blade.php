ba89997a9873fa4e650203365d3a8c664a051511
g0d13 <g0d13@outlook.com>
2021-06-28 10:17:17 -0500

    Eliminar codigo innecesario del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,27 +32,5 @@
             </div>
         </div>
     </section>
-{{--    <div class="container">--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-md-4 offset-md-4">--}}
-{{--                <div class="login-form shadow mt-4 p-4">--}}
-{{--                    <form  class="row g-3">--}}
-{{--                        @csrf--}}
-{{--                        <h4 class="col-12">Bienvenido</h4>--}}
-{{--                        <div class="col-12">--}}
-{{--                            <label for="email">Correo</label>--}}
-{{--                            <input id="email" type="email" name="email" class="form-control" placeholder="Correo" required>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-12 mt-2">--}}
-{{--                            <label for="password">Contraseña</label>--}}
-{{--                            <input id="password" type="password" name="password" class="form-control" placeholder="Contraseña" required>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-12 mt-3">--}}
-{{--                            <button type="submit" class="btn btn-primary float-right">Login</button>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+
 </x-guest-layout>
