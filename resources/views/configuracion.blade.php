be79489f6e35a70ca126a5ad8d32293517dfdf12
Diego Parra <g0d13@outlook.com>
2021-10-29 13:02:30 -0500

    agregar input para url de eventos

--- resources/views/configuracion.blade.php
+++ resources/views/configuracion.blade.php
@@ -4,13 +4,8 @@
     </x-slot>
     <div class="card p-4">
         <form>
-            <label for="usb">Selecciona puerto USB</label>
-            <select id="usb" class="form-select form-select" aria-label=".form-select-sm example">
-                <option selected>Seleccionar</option>
-                <option value="1">COM1</option>
-                <option value="2">COM2</option>
-                <option value="3">COM3</option>
-            </select>
+            <label for="url">Escribe la url para enviar eventos</label>
+            <input type="text" class="form-control"  id="url">
         </form>
     </div>
 </x-app-layout>
